a33a4dad15c9f3a5e2f569fea91b14f5a0804c84
itscool <romeopaolotolentino@gmail.com>
2026-07-13 15:30:15 +0800

    Clean up temp helper

--- get_data.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/database/database.php';
-$reqs = db_all("SELECT * FROM community_service_requirement WHERE student_id = '2023-183482'");
-$sessions = db_all("SELECT * FROM community_service_session WHERE requirement_id = 43");
-header('Content-Type: application/json');
-echo json_encode(['reqs' => $reqs, 'sessions' => $sessions], JSON_PRETTY_PRINT);
