bbe625c8d79b129475d0b371847f5003c8e36cbf
itscool <romeopaolotolentino@gmail.com>
2026-07-25 13:44:09 +0800

    Remove Admin Inactivity Auto-Logout feature as requested

--- admin/logout.php
+++ admin/logout.php
@@ -5,8 +5,4 @@
 require_once __DIR__ . '/../database/database.php';
 
 admin_logout();
-
-if (isset($_GET['reason']) && $_GET['reason'] === 'inactivity') {
-    redirect('login.php?reason=inactivity');
-}
 redirect('logout_success.php');
