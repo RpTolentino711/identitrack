23a1a92f8d9bb398c939e669c6157c754172380f
itscool <romeopaolotolentino@gmail.com>
2026-08-14 02:53:32 +0800

    Fix JS escapeHtml ReferenceError in search modal results list

--- admin/upcc_cases.php
+++ admin/upcc_cases.php
@@ -1957,6 +1957,14 @@ function clearModalSearch(resetAll = false) {
         renderModalSearchResults();
         if (input) input.focus();
     }
+function escapeHtml(str) {
+    if (str === null || str === undefined) return '';
+    return String(str)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;');
 }
 
 function renderModalSearchResults() {
