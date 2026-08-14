42375aab412d456be0769338b37a3e5549396c7b
itscool <romeopaolotolentino@gmail.com>
2026-08-15 07:55:17 +0800

    Configure BLOCK_NONE safetySettings to prevent PROHIBITED_CONTENT blocks on disciplinary case prompts

--- admin/api_ai_suggest_sanction.php
+++ admin/api_ai_suggest_sanction.php
@@ -198,6 +198,13 @@ function callGemini(string $systemPrompt, string $userPrompt): ?string
                 'generationConfig' => [
                     'temperature' => 0.2,
                     'maxOutputTokens' => 3000
+                ],
+                'safetySettings' => [
+                    ['category' => 'HARM_CATEGORY_HARASSMENT', 'threshold' => 'BLOCK_NONE'],
+                    ['category' => 'HARM_CATEGORY_HATE_SPEECH', 'threshold' => 'BLOCK_NONE'],
+                    ['category' => 'HARM_CATEGORY_SEXUALLY_EXPLICIT', 'threshold' => 'BLOCK_NONE'],
+                    ['category' => 'HARM_CATEGORY_DANGEROUS_CONTENT', 'threshold' => 'BLOCK_NONE'],
+                    ['category' => 'HARM_CATEGORY_CIVIC_INTEGRITY', 'threshold' => 'BLOCK_NONE']
                 ]
             ];
 
