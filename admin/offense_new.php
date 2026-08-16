0d2e6d5330e6de0fbd3078e5689174e60308599c
itscool <romeopaolotolentino@gmail.com>
2026-08-17 01:09:20 +0800

    Enable error reporting display_errors on offense_new.php

--- admin/offense_new.php
+++ admin/offense_new.php
@@ -1,5 +1,8 @@
 <?php
 // File: admin/offense_new.php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 require_once __DIR__ . '/../database/database.php';
 require_admin();
 
