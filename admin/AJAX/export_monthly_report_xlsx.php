d23b9f686ed7ab44004378a8aa39dba4c98f5839
itscool <romeopaolotolentino@gmail.com>
2026-09-18 03:29:28 +0800

    fix(export): remove heavy chart XML serialization to prevent 504 Gateway Timeout on Nginx

--- admin/AJAX/export_monthly_report_xlsx.php
+++ admin/AJAX/export_monthly_report_xlsx.php
@@ -1099,7 +1099,6 @@ try {
   header('Cache-Control: max-age=0');
   
   $writer = new Xlsx($spreadsheet);
-  $writer->setIncludeCharts(true);
   $writer->setPreCalculateFormulas(false);
   $writer->save('php://output');
   if (php_sapi_name() !== 'cli') {
